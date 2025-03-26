b28bcba3c6097d8d6f4d0133995528ebb5e09ce2
Roshan <91822433+roshansalunke@users.noreply.github.com>
2025-03-26 18:16:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS ECS ! ");
+  res.send("Hello frosssm AWS ECS ! ");
 });
 
 app.listen(3000, () => {
